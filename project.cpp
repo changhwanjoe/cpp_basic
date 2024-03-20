1f88ebf760325d6c01cb07a22fdb1728d3fecd27
changhwan <chjoe01@gmail.com>
2024-03-21 08:12:15 +0900

    project added

---
+++ project.cpp
@@ -0,0 +1,29 @@
+// 과제는 여기에 제출해 주세요
+// 아래의 형태로 제출해 주세요
+
+// 1번
+// 소스
+#include <iostream>
+namespace utils{
+	template <typename T>
+
+	T min(T a, T b){
+		if (a>=b) return b;
+		else return a;
+	}
+}
+
+int main(){
+	std::cout << utils::min(3,4) << std::endl;
+	std::cout << utils::min(3.1,2.3) <<std::endl;
+}
+
+// 2번 
+// 소스
+
+
+// 3번
+// 소스
+
+// 4번
+// 소스
