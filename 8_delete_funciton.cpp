2116b75b071acd1ae7f93e11bee3ee833573bc0b
changhwan <chjoe01@gmail.com>
2024-03-21 09:14:44 +0900

    chap 8 9 added

--- 8_delete_funciton.cpp
+++ 8_delete_funciton.cpp
@@ -5,5 +5,13 @@
 
 int gcd(int a, int b)
 {
-    return b != 0 ? gcd(b, a)
+    return b != 0 ? gcd(b, a%b) : a;
+
+}
+
+double gcd(double a, double b) = delete; //함수를 사요앟ㄹ 수 없다는 comepile error생성 
+int main()
+{
+    gcd(10,4);
+    gcd(3.3,4.4);
 }
